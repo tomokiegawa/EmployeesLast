f86d8d5f4852f13d969221f35084052c3fadce46
tomokiegawa <egawa@seamaple.co.jp>
2024-05-21 15:25:27 +0900

    編集機能完成

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -6,24 +6,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.impl.UserService;
+import com.example.form.UserDetailForm;
 
 @Controller
+@RequestMapping("/index")
 public class IndexController {
 	@Autowired
 	private UserService userService;
 
-	/** ユーザー⼀覧画⾯を表⽰ */
-	@GetMapping("/index")
+	@GetMapping
 	public String getUserList(Model model) {
 		// ユーザー⼀覧取得
 		List<MUser> userList = userService.getUsers();
 		// Modelに登録
 		model.addAttribute("userList", userList);
+		// model.addAttribute("userDetailForm", new UserDetailForm()); // 空のフォームを追加
 		// ユーザー⼀覧画⾯を表⽰
 		return "index";
 	}
+	
+	/** ユーザー削除処理 */
+	@PostMapping(value = "/index", params = "delete")
+	public String deleteUser(UserDetailForm form, Model model) {
+	    userService.deleteUserOne(form.getEmployeeId());
+	    model.addAttribute("userList", getUserList(null));
+	    return "redirect:/index";
+	}
+	
+	
+	
+//	@GetMapping
+//    public String getRegist(Model model, @ModelAttribute RegistForm form) {
+//        return "regist"; 
+//    }
+	
 }
 
--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -1,8 +1,10 @@
 package com.example.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.domain.user.model.MUser;
 
@@ -13,4 +15,17 @@ public interface UserMapper {
 	public int insertOne(MUser user);
 	/** ユーザー取得 */
 	public List<MUser> findMany();
+	/**ユ－ザー取得(１件)*/
+public MUser findOne(int employeeId);
+	
+	/** ユーザー更新(1件) */
+	public void updateOne(@Param("employeeId") int employeeId,
+	@Param("employeePass") String employeePass,
+	@Param("employeeName") String employeeName,
+	@Param("mail")String mail,
+	@Param("phone")String phone,
+	@Param("anniversary")Date anniversary);
+	
+	/** ユーザー削除(1件) */
+	public int deleteOne(@Param("employeeId") int employeeId);
 }
