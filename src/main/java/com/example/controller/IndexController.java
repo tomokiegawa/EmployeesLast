b07b61c065be39d48a2d04da48a123cd7e9b0e36
tomokiegawa <egawa@seamaple.co.jp>
2024-05-21 16:39:38 +0900

    1212

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.example.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.impl.UserService;
+import com.example.form.UserDetailForm;
 
 @Controller
 @RequestMapping("/index")
@@ -21,8 +24,16 @@ public class IndexController {
 	public String getUserList(Model model) {
 		// ユーザー⼀覧取得
 		List<MUser> userList = userService.getUsers();
+		List<UserDetailForm> userDetailFormList = new ArrayList<>();
+		
+		for(MUser user : userList) {
+			UserDetailForm userDetailForm = new UserDetailForm();
+			BeanUtils.copyProperties(user, userDetailForm);
+			userDetailFormList.add(userDetailForm);
+		}
+		
 		// Modelに登録
-		model.addAttribute("userList", userList);
+		model.addAttribute("userList", userDetailFormList);
 		// model.addAttribute("userDetailForm", new UserDetailForm()); // 空のフォームを追加
 		// ユーザー⼀覧画⾯を表⽰
 		return "index";
